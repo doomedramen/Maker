e77943783600d698032108a9639012a44b410989
Martin Page <wookoouk@gmail.com>
2019-01-11 09:22:18 +0000

    stuff

--- src/scenes/Make/Make.gd
+++ src/scenes/Make/Make.gd
@@ -6,25 +6,49 @@ var current_grid_position = Vector2()
 
 var lmb_down = false
 
+const CAMERA_MOVE_SPEED_STEP = 100
+
 #var Ememy = load("res://src/sprites/Enemy.tscn")
 
 var currentItem
+var zoom_level = 1
 #onready var currentItem = Ememy.instance()
 
 func _ready():
 	add_child(currentItem)
 	get_tree().get_root().connect("size_changed", self, "update")
+	
+	#snap camera
+	#$Camera2D.position = pos_to_cell($Camera2D.position)
 
-func _draw():
+func pos_to_cell(vec2):
+	var out = Vector2();
+	out.x = round(vec2.x / GRID_SIZE) * GRID_SIZE;
+	out.y = round(vec2.y / GRID_SIZE) * GRID_SIZE;
+	return out
+	
+func draw_grid():
 	var projectResolution=get_viewport().size
 	var widest = projectResolution.x if projectResolution.x > projectResolution.y else projectResolution.y
-	var max_lines = (widest / GRID_SIZE)+1 #avoid crash if size too smal
+	var max_lines = (widest / GRID_SIZE)*2 #avoid crash if size too smal
 
-	for x in range(0, max_lines):
-		x = x*GRID_SIZE
-		draw_line(Vector2(x,0), Vector2(x, projectResolution.y), Color(1, 1, 1,.5), 1)
-		draw_line(Vector2(0,x), Vector2(projectResolution.x, x), Color(1, 1, 1, .5), 1)
+
+	var padding = GRID_SIZE*2
+	var start = $Camera2D.position - (projectResolution/2) - Vector2(padding, padding)
+	var end = Vector2(start.x + projectResolution.x, start.y + projectResolution.y) + (Vector2(padding, padding)*2)
 		
+	var startCell = pos_to_cell(start)
+	var endCell = pos_to_cell(end)
+	
+	for i in range(0, max_lines):
+		i = GRID_SIZE*i
+		
+		draw_line(Vector2(startCell.x,startCell.y+i), Vector2(endCell.x, startCell.y+i), Color(1, 1, 1,.5), 1)
+		draw_line(Vector2(startCell.x+i,startCell.y), Vector2(startCell.x+i, endCell.y), Color(1, 1, 1,.5), 1)
+		
+
+func _draw():
+
 	var currentCell = Rect2(current_grid_position,Vector2(GRID_SIZE,GRID_SIZE))
 	draw_rect(currentCell, Color(0, 0, 1,.3), 1)
 
@@ -35,22 +59,30 @@ func _input(ev):
 		if currentItem:
 			add_child(currentItem.duplicate())
 		
-    # Do stuff...
 	if ev is InputEventMouseMotion:
 		var mouse_pos = get_global_mouse_position();
+		mouse_pos -= Vector2(GRID_SIZE/2, GRID_SIZE/2)
+
+		current_grid_position = pos_to_cell(mouse_pos)
 		
-		mouse_pos.x -=GRID_SIZE/2 #fix center bug
-		mouse_pos.y -=GRID_SIZE/2 #fix center bug
-		
-		current_grid_position.x = round(mouse_pos.x / GRID_SIZE) * GRID_SIZE;
-		current_grid_position.y = round(mouse_pos.y / GRID_SIZE) * GRID_SIZE;
-		
-		if currentItem:
-			#get tile center
-			var center_of_cell = Vector2(current_grid_position.x+(GRID_SIZE/2), current_grid_position.y+(GRID_SIZE/2))
-			currentItem.position = center_of_cell
+		#if currentItem:
+		#	var center_of_cell = Vector2(current_grid_position.x+(GRID_SIZE/2), current_grid_position.y+(GRID_SIZE/2))
+		#	currentItem.position = center_of_cell
 		
 		if lmb_down:
 			print("we draggin!")
+			
 		
-		update() #rather important...
+	if ev is InputEventKey:
+		if Input.is_action_pressed("ui_left"):
+			$Camera2D.translate(Vector2(-CAMERA_MOVE_SPEED_STEP, 0))
+		if Input.is_action_pressed("ui_right"):
+			$Camera2D.translate(Vector2(CAMERA_MOVE_SPEED_STEP, 0))
+		if Input.is_action_pressed("ui_up"):
+			$Camera2D.translate(Vector2(0,-CAMERA_MOVE_SPEED_STEP))
+		if Input.is_action_pressed("ui_down"):
+			$Camera2D.translate(Vector2(0,CAMERA_MOVE_SPEED_STEP))
+		
+	
+	
+	update() #rather important...
